e09d5f76d03e824d2861339fc7193e02f363670d
Megalapa <megalapa@MacBook-Air-Megalapa.local>
2015-04-09 17:46:42 +0300

    new commiting

--- animat.js
+++ animat.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
 
 	$(".conti2").fadeOut(1);
-	$(".conti3").fadeOut(1);
-	$(".conti4").fadeOut(1);
+	$(".conti3").fadeOut(1);	
 	$(".conti5").fadeOut(1);
 	$(".conti6").fadeOut(1);
 	$("li").click(function () {
@@ -13,7 +12,6 @@ case main:
 	$(".conti1").fadeIn(600);
 	$(".conti2").fadeOut();
 	$(".conti3").fadeOut();
-	$(".conti4").fadeOut();
 	$(".conti5").fadeOut();
 	$(".conti6").fadeOut();
 	breake;
@@ -22,7 +20,6 @@ case about:
 	$(".conti1").fadeOut();
 	$(".conti2").fadeIn(600);
 	$(".conti3").fadeOut();
-	$(".conti4").fadeOut();
 	$(".conti5").fadeOut();
 	$(".conti6").fadeOut();
 	breake;
@@ -31,34 +28,23 @@ case services:
 	$(".conti1").fadeOut();
 	$(".conti2").fadeOut();
 	$(".conti3").fadeIn(600);
-	$(".conti4").fadeOut();
 	$(".conti5").fadeOut();
 	$(".conti6").fadeOut();
 	breake;
-case clients:
-	$("#polzun").animate({left: '614px'});
-	$(".conti1").fadeOut();
-	$(".conti2").fadeOut();
-	$(".conti3").fadeOut();
-	$(".conti4").fadeIn(600);
-	$(".conti5").fadeOut();
-	$(".conti6").fadeOut();
-	breake;
+
 case contacts:
-	$("#polzun").animate({left: '743px'});
+	$("#polzun").animate({left: '593px'});
 	$(".conti1").fadeOut();
 	$(".conti2").fadeOut();
 	$(".conti3").fadeOut();
-	$(".conti4").fadeOut();
 	$(".conti5").fadeIn(600);
 	$(".conti6").fadeOut();
 	breake;
 case vacancies:
-	$("#polzun").animate({left: '852px'});
+	$("#polzun").animate({left: '702px'});
 	$(".conti1").fadeOut();
 	$(".conti2").fadeOut();
 	$(".conti3").fadeOut();
-	$(".conti4").fadeOut();
 	$(".conti5").fadeOut();
 	$(".conti6").fadeIn(600);
 	
@@ -68,7 +54,6 @@ case vacancies:
 	$(".conti1").fadeIn(600);
 	$(".conti2").fadeOut();
 	$(".conti3").fadeOut();
-	$(".conti4").fadeOut();
 	$(".conti5").fadeOut();
 	$(".conti6").fadeOut();
 	breake;
